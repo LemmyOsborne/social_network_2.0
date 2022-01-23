132f4b378c5f067cdb2de95ebe07d6b15a0f314e
lemmyosborne <bozhenko.rodion@mail.ru>
2022-01-23 15:44:36 +0300

    Create hoc which checking if user is authorized and if not redirect to login page

---
+++ src/hoc/withAuthRedirect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+
+
+export const withAuthRedirect = ({children}) => {
+
+    const isAuth = useSelector(state => state.auth.isAuth)
+
+    if (!isAuth) return <Navigate to="/login" />
+    return children
+}
+
