08659bb1a2eb2a131b0542b55e314ef69546a856
lemmyosborne <bozhenko.rodion@mail.ru>
2022-02-04 13:12:07 +0300

    Migrate hoc to ts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react"
 import "./App.css"
 import { Profile } from "./components/profile/Profile"
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
--- src/hoc/WithAuthRedirect.tsx
+++ src/hoc/WithAuthRedirect.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import { Navigate } from "react-router-dom"
 import { useAppSelector } from "../store/hooks"
+ 
+interface Props {
+    children: React.ReactNode 
+}
 
-
-export const WithAuthRedirect = ({ children }) => {
+export const WithAuthRedirect = ({ children }: Props) => {
     const { isAuth } = useAppSelector(state => state.auth)
     if (!isAuth) return <Navigate to="/login"/>
 
